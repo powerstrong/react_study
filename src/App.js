6751ced0becd108c80eb7b0dcba36cff50ae7aa4
jslee <jslee@plaif.com>
2022-11-26 22:32:48 +0900

    5강 : Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '이주성',
-  'birthday': '850504',
-  'gender': '남자',
-  'job': '아저씨'
-}
+const customers = [
+  {
+    'id':123,
+    'image':'https://placeimg.com/64/64/1', // 이미지크기 64*64 랜덤
+    'name': '이주성',
+    'birthday': '850504',
+    'gender': '남자',
+    'job': '아저씨'
+  },
+  {
+    'id':124,
+    'image':'https://placeimg.com/64/64/2',
+    'name': '이선미',
+    'birthday': '812345',
+    'gender': '여자',
+    'job': '아줌마'
+  },
+  {
+    'id':125,
+    'image':'https://placeimg.com/64/64/3',
+    'name': '이다은',
+    'birthday': '812333',
+    'gender': '여자',
+    'job': '언니'
+  }
+]
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      { customers.map(c => { return (
+            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          );
+        })
+      }
+    </div>
   );
 }
 
